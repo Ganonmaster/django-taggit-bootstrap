d4faec0e1e12be57724c2b6a01b0052def4aa75c
Ganonmaster <admin@sfmlab.com>
2017-01-14 18:16:15 +0100

    Updated bootstrap-tagsinput.js

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 setup(name='django-taggit-bootstrap',
-      version='0.1',
-      description='Django Taggit widget with autosuggest support on top of Bootstrap 3',
-      author='Michael Boyarov',
-      author_email='mi666gan@gmail.com',
-      url='https://github.com/mi6gan/django-taggit-bootstrap',
+      version='0.2',
+      description='Django Taggit widget with autosuggest support on top of Bootstrap 3 - updated with latest bootstrap-tagsinput',
+      author='Hidde Jansen',
+      author_email='hidde@hiddejansen.com',
+      url='https://github.com/Ganonmaster/django-taggit-bootstrap',
       packages=['taggit_bootstrap'],
       package_data={'taggit_bootstrap':["templates/taggit_bootstrap/*.html","static/css/*.css","static/js/*.js"]},
       requires=['django_taggit']
